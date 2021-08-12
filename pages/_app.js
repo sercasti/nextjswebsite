85662eeecd7062af7848af06a4888dd6b3a82937
Castineyras <scastine@147dda3a7fb4.ant.amazon.com>
2021-08-12 17:46:29 -0300

    added backend

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import Amplify from 'aws-amplify';
+import config from '../src/aws-exports';
+Amplify.configure({
+  ...config, ssr: true
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
